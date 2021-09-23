54d9b6afcfb1846287e8b88466ccd42a18d8bcfb
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-09-23 16:06:20 -0400

    Had to redo exercise26 to include new required 'program skeleton'

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,16 @@
+package baseline;
+
+public class PaymentCalculator {
+
+    // balance;
+    // apr;
+    // dailyRate;
+    // monthlyPayment;
+
+    // public PaymentCalculator(balance, apr, monthlyPayment)
+
+    // private double roundUpPenny(value)
+
+    // public int calculateMonthsUntilPaidOff()
+
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+public class Solution26 {
+
+    // create PaymentCalculator class
+
+    // private static void main(String[] args)
+
+    // private double getInputFromUser(prompt)
+
+    // private void displayMonthsToPay(numberMonths)
+}
