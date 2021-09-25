e40a7cdaa2989578a0770cc110721ef2266d943d
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-09-25 14:27:27 -0400

    Made the skeleton code for the solution to exercise28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+public class Solution28 {
+
+    public static void main(String[] args) {
+        // Write a program that prompts the user for five numbers and computes the total of the numbers.
+
+        // prompt user for numbers
+        // add those numbers together
+        // print the total to the user
+    }
+
+    private int addNumbersFromUser(int amountOfNumbers) {
+        // for the amount of numbers we have, get that many from the user
+        // using promptUserForNumber() the amountOfNumbers times
+
+        // placeholder
+        return 0;
+    }
+
+    private int promptUserForNumber() {
+        // prompt user to enter a number
+        // get the number from the user
+        // return the number
+
+        // placeholder
+        return 0;
+    }
+
+    private void displayTotal(int total) {
+        // print the total to the user
+    }
+}
