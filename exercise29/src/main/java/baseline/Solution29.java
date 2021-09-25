886043d3dbe49e05b4a0b868c13870728f0fc5b0
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-09-25 17:21:13 -0400

    Completed the skeleton code for exercise29

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,51 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+public class Solution29 {
+
+    public static void main(String[] args) {
+        // Prompt user for rate of return
+        // Check if their input is valid
+        // If not valid, tell them, then prompt again
+        // Once valid, calculate how long to double investment
+        // Display the length it will take to the user
+    }
+
+    private void displayLengthToDoubleInvestment(double years) {
+        // print how long it will take to double the investment
+    }
+
+    private double calculateLengthToDoubleInvestment(double rate) {
+        // use formula years = 72 / r
+        // return this number
+
+        // placeholder
+        return 0;
+    }
+
+    private double getRateOfReturnFromUser() {
+        // Prompt the user for the rate of return
+        // Check that the rate is a number
+        // Check that the rate is not 0
+
+
+        // Use a loop to check for bad input
+        // Return the rate of return
+
+        // placeholder
+        return 0;
+    }
+
+    private boolean isRateValid(String number) {
+        // check if rate is number
+        // check if rate is not 0
+        // return whether it is valid using these rules
+
+        // placeholder
+        return true;
+    }
+}
