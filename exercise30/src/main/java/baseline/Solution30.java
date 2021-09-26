63cf631a30c0e11e0699a4a38f149341da4c85be
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-09-25 20:33:48 -0400

    Made basic skeleton for exercise30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+public class Solution30 {
+
+    public static void main(String[] args) {
+        // generate multiplication table
+    }
+
+    public void generateTable() {
+        // use a nested for loop
+        // go from 1-12 in each loop
+        // aligning each column
+        // build the string
+        // print the string
+    }
+}
