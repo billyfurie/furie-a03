0c0458d7a9bdd99998fb8086ed8b74c046f462b7
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-09-28 11:28:37 -0400

    Finished skeleton to exercise33

---
+++ exercise32/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+Y
+)src/test/java/baseline/GuessGameTest.java,2\5\254318be9467e1965a775a72b497fd95a7f505a0
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+U
+%src/main/java/baseline/GuessGame.java,7\d\7da1b1d411a07974b8113be2a18a252e75d75431
+V
+&src/main/java/baseline/Solution32.java,b\d\bd879f0038f65cacac80a75883d242ae15f31cbf
---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+public class Solution33 {
+
+    // have constants for the possible answers
+
+    public static void main(String[] args) {
+        // create our answer bank (not user based)
+
+        // get the question from the user
+        // display answer from our answer bank
+    }
+
+    private void displayAnswer(String[] answerBank) {
+        // index is random int
+        // display the answer at that index
+    }
+
+    private String[] createAnswerBank(String ... answers) {
+        // create a list of answers
+        // return the list
+
+        // placeholder
+        return null;
+    }
+
+    private int getRandomInt(int range) {
+        // use library to return a random integer from 0 to n
+
+        // placeholder
+        return 0;
+    }
+
+    private void getQuestionFromUser() {
+        // prompt for question
+        // we actually don't care to store the users question.
+    }
+}
