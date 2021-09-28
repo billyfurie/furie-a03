c229c292f8886a3ceef19f6955ae4864ab0a0f9a
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-09-28 11:58:00 -0400

    Finished solution to exercise33 with unit testing included.

--- exercise33/src/main/java/baseline/Solution33.java
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -5,39 +5,75 @@
 
 package baseline;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Solution33 {
 
     // have constants for the possible answers
+    private static final String ANSWER_YES = "Yes.";
+    private static final String ANSWER_NO = "No.";
+    private static final String ANSWER_MAYBE = "Maybe.";
+    private static final String ANSWER_NOT_NOW = "Ask again later.";
+    private static final int NUM_ANSWERS = 4;
+
+    private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+        Solution33 solution = new Solution33();
+
         // create our answer bank (not user based)
+        String[] bank = solution.createAnswerBank(ANSWER_YES, ANSWER_NO, ANSWER_MAYBE, ANSWER_NOT_NOW);
 
         // get the question from the user
+        solution.getQuestionFromUser();
+
+        // get the answer (does not actually rely on question)
+        String answer = solution.getAnswer(bank);
+
         // display answer from our answer bank
+        solution.displayAnswer(answer);
     }
 
-    private void displayAnswer(String[] answerBank) {
+    private void displayAnswer(String answer) {
+        // We want a line of space between question and answer
+        System.out.printf("%n%s", answer);
+    }
+
+    public String getAnswer(String[] answerBank) {
+        if (answerBank == null) {
+            return "";
+        }
+
         // index is random int
-        // display the answer at that index
+        int index = getRandomInt(NUM_ANSWERS);
+        // return the answer of our random index
+        return answerBank[index];
     }
 
-    private String[] createAnswerBank(String ... answers) {
+    public String[] createAnswerBank(String ... answers) {
+        String[] bank = new String[answers.length];
         // create a list of answers
-        // return the list
+        int index = 0;
+        for (String answer : answers) {
+            bank[index++] = answer;
+        }
 
-        // placeholder
-        return null;
+        // return the list
+        return bank;
     }
 
-    private int getRandomInt(int range) {
+    public int getRandomInt(int range) {
         // use library to return a random integer from 0 to n
+        Random random = new Random();
 
-        // placeholder
-        return 0;
+        return random.nextInt(range);
     }
 
     private void getQuestionFromUser() {
         // prompt for question
+        System.out.print("What's your question?\n> ");
         // we actually don't care to store the users question.
+        input.nextLine();
     }
 }
---
+++ exercise33/src/test/java/baseline/Solution33Test.java
@@ -0,0 +1,59 @@
+package baseline;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution33Test {
+
+    @Test
+    void getAnswer() {
+        Solution33 solution = new Solution33();
+
+        String[] values = {"Hello", "This", "Is", "A", "Test"};
+
+        for (int i = 0; i < 50; i++) {
+            String value = solution.getAnswer(values);
+
+            // make sure the result we got randomly is in the array
+            assert Arrays.asList(values).contains(value);
+        }
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sourcesCreateAnswerBank.csv", numLinesToSkip = 1)
+    void createAnswerBank(String a, String b, String c) {
+        Solution33 solution = new Solution33();
+
+        String[] expectedOne = {a};
+        String[] actualOne = solution.createAnswerBank(a);
+
+        String[] expectedTwo = {a, b};
+        String[] actualTwo = solution.createAnswerBank(a, b);
+
+        String[] expectedThree = {a, b, c};
+        String[] actualThree = solution.createAnswerBank(a, b, c);
+
+        System.out.println(actualOne + ", " + expectedOne);
+
+        assertTrue(Arrays.equals(actualOne, expectedOne));
+        assertTrue(Arrays.equals(actualTwo, expectedTwo));
+        assertTrue(Arrays.equals(actualThree, expectedThree));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sourcesGetRandomInt.csv", numLinesToSkip = 1)
+    void getRandomInt(int range) {
+        Solution33 solution = new Solution33();
+
+        for (int i = 0; i < 50; i++) {
+            int actual = solution.getRandomInt(range);
+
+            assert actual >=0 && actual < range;
+        }
+    }
+}
