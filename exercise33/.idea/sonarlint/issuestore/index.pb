049dd8a3e6f3195ce86e04c6dc8af28244b28e7e
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-02 09:23:30 -0400

    Finished the skeleton code for exercise34

---
+++ exercise33/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+Z
+*src/test/java/baseline/Solution33Test.java,d\3\d3e7eb4d1cae1258ff1a96ad1824e82685032ee0
---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,46 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+import java.util.List;
+
+public class Solution34 {
+
+    public static void main(String[] args) {
+        // create our list of employees
+        // display the list to the user
+        // prompt the user for an employee to remove
+        // delete the employee if they exist
+        // display the updated list
+    }
+
+    private List<String> createListOfEmployees(String ... employees) {
+        // add each of the employees to our list
+        // return the list
+
+        // placeholder
+        return null;
+    }
+
+    private void displayListOfEmployees(List<String> list) {
+        // print out the list of employees
+    }
+
+    private String getEmployeeToRemove() {
+        // prompt the user
+        // get a name from the user
+        // return the name
+
+        // placeholder
+        return "";
+    }
+
+    private void deleteEmployee(List<String> list) {
+        // check if employee is in list
+        // if they are not, display a message telling the user
+        // if they are, we remove them
+    }
+}
