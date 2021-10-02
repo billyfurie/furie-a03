561e8f6b33f076a376698b978df84f44ea77286d
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-02 12:51:10 -0400

    Finished skeleton code for exercise36

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,79 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+import java.util.List;
+
+public class Solution36 {
+
+    public static void main(String[] args) {
+        // get the response times from the user
+        // display the response times
+        // calculate the average, minimum, maximum, and standard deviation
+    }
+
+    private void displayResults(List<Double> numbers, double average, double min, double max, double std) {
+        // display the results to the user
+    }
+
+    private double std(List<Double> numbers) {
+        // calculate the std dev of the numbers
+        // return the std dev
+
+        // placeholder
+        return 0;
+    }
+
+    private double max(List<Double> numbers) {
+        // find the maximum of the numbers
+        // return the maximum
+
+        // placeholder
+        return 0;
+    }
+
+    private double min(List<Double> numbers) {
+        // find the minimum of the numbers
+        // return the minimum
+
+        // placeholder
+        return 0;
+    }
+
+    private double average(List<Double> numbers) {
+        // calculate the average of the numbers
+        // return the average
+
+        // placeholder
+        return 0;
+    }
+
+    private boolean isNumber(String num) {
+        // return whether the string is numeric
+
+        // placeholder
+        return true;
+    }
+
+    private List<Double> buildResponseTimesList() {
+        // create the list
+        // keep getting numbers from user
+        // verify that the string is a number before we add
+        // if they enter done, we stop
+        // return the list
+
+        // placeholder
+        return null;
+    }
+    
+    private String getNumber() {
+        // prompt for the number
+        // return the number
+
+        // placeholder
+        return "";
+    }
+ }
