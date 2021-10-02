7f65ee85d9b1813e9bbad9f70229011eccedef23
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-02 19:49:31 -0400

    Finished skeleton code for exercise38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,39 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+public class Solution38 {
+
+    public static void main(String[] args) {
+        // prompt user for a list of number separated by spaces
+        // ensure that each character is either a number or a space
+        // convert to an array
+        // filter out only the even numbers in the array and generate a new array
+        // display the even numbers to the user
+    }
+
+    private char[] getListStringFromUser() {
+        // print the prompt
+        // return the string as an array
+
+        // placeholder
+        return null;
+    }
+
+
+    private int[] filterEvenNumbers(char[] charArray) throws IllegalArgumentException {
+        // go through the string and parse the numbers adding them to an array
+        // if the string is not all numeric / spaces, throw an exception
+        // return the filtered array
+
+        // placeholder
+        return null;
+    }
+
+    private void displayEvenNumbers(int[] evens) {
+        // print out the even numbers
+    }
+}
