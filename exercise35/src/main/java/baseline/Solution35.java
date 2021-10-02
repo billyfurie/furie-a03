d2bee60eb83c68503a4996bec6805cbb91a626da
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-02 12:05:37 -0400

    Finished skeleton code to exercise35

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,51 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 William Furie
+ */
+
+package baseline;
+
+import java.util.List;
+
+public class Solution35 {
+
+    public static void main(String[] args) {
+        // get the contestants from the user
+        // generate the winner
+        // display the winner
+    }
+
+    private void displayWinner(String winner) {
+        // print the winner message
+    }
+
+    private String getWinner(List<String> contestants) {
+        // generate a random number in the range of length of contestants
+        // return the contestant at that index
+
+        // placeholder
+        return "";
+    }
+
+    private List<String> buildContestantsList() {
+        // keep getting contestant names
+        // add contestant to our list as long as the name is not blank
+        // if the name is blank we stop
+        // return the list
+
+        // placeholder
+        return null;
+    }
+
+    private void addContestant(List<String> contestants, String contestant) {
+        // add contestant to the list
+    }
+
+    private String getContestant() {
+        // get the contestants name from the user
+        // return the contestants name
+
+        // placeholder
+        return "";
+    }
+}
