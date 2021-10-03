d652bb35c1435599ed26b845ed838e24f823ce2d
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-10-03 14:40:26 -0400

    Finished the skeleton code for exercise40. Almost donegit add . !

---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,58 @@
+package baseline;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution40 {
+
+    public static void main(String[] args) {
+        // create a list of the employee maps
+        // get the search string from the user
+        // filter out the results using the search string and the employee name
+        // display the results to the user
+    }
+
+    private List<HashMap<Integer, String>> createEmployeeMapList() {
+        // create a list of maps
+        // make a map for each of the employees
+        // add all the maps to the list
+        // return the list
+
+        // placeholder
+        return null;
+    }
+
+    private String getSearchFromUser() {
+        // prompt the user for a search string
+        // get and return this string
+
+        // placeholder
+        return "";
+    }
+
+    private List<HashMap<Integer, String>> getFilteredListFromSearch(String search) {
+        // create a list for the filtered employees
+
+        // go through the list of maps
+        // in each of the maps, analyze the first and last name
+        // check if the search string exists in either string
+        // if it does, we will add this employee to our filtered list
+
+        // return the filtered list
+
+        // placeholder
+        return null;
+    }
+
+    private boolean isSearchMatch(String search, String word) {
+        // return whether the search is inside the word
+
+        // placeholder
+        return false;
+    }
+
+    private void displayFilteredResults(List<HashMap<Integer, String>> list) {
+        // check if the list is empty
+        // if it is not display the filtered records to the user
+    }
+}
